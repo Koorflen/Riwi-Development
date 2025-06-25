2ba3d17b368eda44187566dbff02a18d1b1dd0bb
Alejandro <sr.alejandrorojas@gmail.com>
2025-06-25 13:17:13 -0500

    25/06

--- Module 3/Repo andrea/Week 1/exercices/4/4.3.js
+++ Module 3/Repo andrea/Week 1/exercices/4/4.3.js
@@ -1,5 +1,10 @@
-console.log(5 == "5")
-console.log(5 === "5")
-console.log(true && false)
-console.log(false || true)
-console.log(!true)
+console.log(5 == "5") // Igualdad
+console.log(5 === "5") // Igualdad Estricta
+console.log(true && false) // and
+console.log(true && true)
+console.log(false && false)
+console.log(false || true) // or
+console.log(true || true)
+console.log(false || false)
+console.log(!true) // not
+console.log(!false)
---
+++ Module 3/Repo andrea/Week 1/exercices/5/5.1.js
@@ -0,0 +1,9 @@
+let number = prompt("Take a number:");
+
+if (number<10) {
+    console.log("You are young is older that 10")
+} else if (number==10) {
+    console.log("your number is 16")
+} else {
+    console.log("You are older is older that 10")
+}
